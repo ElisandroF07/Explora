afe77847b2c305ea77dd019e6598b5e8da201163
ElisandroF07 <f07nobru@gmail.com>
2023-09-11 00:22:27 +0100

    Feat: Regex on inputs

--- src/app/(pages)/signIn/page.tsx
+++ src/app/(pages)/signIn/page.tsx
@@ -1,43 +1,93 @@
+'use client';
 
-
-import { FaFacebook } from 'react-icons/fa6';
+import { FaFacebook, FaCircleInfo } from 'react-icons/fa6';
 import google from '@/assets/images/googleLogo.jpg';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useState } from 'react';
 
 export default function SignIn() {
+	const [password, setPassword] = useState('');
+	const [email, setEmail] = useState('');
+
+	function check(): void {
+		let inputControls = document.querySelectorAll(
+			'.inputControl'
+		) as NodeListOf<HTMLDivElement>;
+		inputControls?.forEach((inputControl) => {
+			let input = inputControl.querySelector('.input') as HTMLInputElement;
+			if (input?.value === '') {
+				input.style.borderColor = '#ff0000';
+				let warningIcon = inputControl.querySelector('.warning') as HTMLElement;
+				warningIcon?.classList.remove('opacity-0');
+			} else {
+				console.log(input);
+			}
+		});
+	}
+
+	function resetInput(event: React.FocusEvent<HTMLInputElement>): void {
+		let input = event.target as HTMLInputElement;
+		let warningIcon = input.parentElement?.querySelector(
+			'.warning'
+		) as HTMLElement;
+		warningIcon?.classList.add('opacity-0');
+		input.style.borderColor = '#aaa';
+	}
+
 	return (
 		<main className='bg-[var(--background-body-introduction)] text-[var(--foreground-introduction)] w-full h-full p-[15px]'>
 			<div>
 				<h1
 					className='font-bold text-[24px] w-[70%] mt-[30px]'
-					style={{ lineHeight: '20px' }}> Hi, Wellcome Back!</h1>
+					style={{ lineHeight: '20px' }}
+				>
+					{' '}
+					Hi, Wellcome Back!
+				</h1>
 				<p className='font-light text-[14px] mt-[10px] text-[#5a5a5a] text-[13px]'>
-					Hello again, you've benn missed!
+					Hello again, you've been missed!
 				</p>
 			</div>
 			<form className='mt-[30px] flex flex-col gap-[20px]'>
-				<div className='inputControl flex flex-col gap-[5px]'>
-					<label htmlFor='email' className='font-semibold text-[14px]'> Email Address </label>
+				<div className='inputControl flex flex-col gap-[5px] relative'>
+					<label htmlFor='email' className='font-semibold text-[14px]'>
+						{' '}
+						Email Address{' '}
+					</label>
 					<input
-						className='input w-full h-[50px] outline-none rounded-md pl-[20px]'
+						className='input w-full h-[50px] transition-colors duration-300 outline-none rounded-md px-[20px]'
 						style={{ border: '1px solid #aaa' }}
 						type='email'
 						name='email'
 						placeholder='Enter your email'
+						onChange={(event) => {
+							setEmail(event.target.value);
+						}}
+						onFocus={(event) => {
+							resetInput(event);
+						}}
 					/>
+					<FaCircleInfo className='warning text-red-500 opacity-0 transition-opacity duration-300 w-[20px] h-[20px] absolute right-[18px] top-[42px]' />
 				</div>
-				<div className='inputControl flex flex-col gap-[5px]'>
+				<div className='inputControl flex flex-col gap-[5px] relative'>
 					<label htmlFor='password' className='font-semibold text-[14px]'>
 						Password
 					</label>
 					<input
-						className='input w-full h-[50px] outline-none rounded-md pl-[20px]'
+						className='input w-full h-[50px] transition-colors duration-300 outline-none rounded-md px-[20px]'
 						style={{ border: '1px solid #aaa' }}
 						type='password'
 						name='password'
 						placeholder='Enter your password'
+						onChange={(event) => {
+							setPassword(event.target.value);
+						}}
+						onFocus={(event) => {
+							resetInput(event);
+						}}
 					/>
+					<FaCircleInfo className='warning text-red-500 opacity-0 transition-opacity duration-300 w-[20px] h-[20px] absolute right-[18px] top-[42px]' />
 				</div>
 				<div className='inputControl flex justify-between'>
 					<div className='flex gap-[10px] items-center justify-start'>
@@ -51,12 +101,18 @@ export default function SignIn() {
 						</label>
 					</div>
 					<div>
-						<p className='font-semibold text-[14px] text-red-500'>
+						<p className='font-semibold text-[14px] text-red-400'>
 							Forgot Password
 						</p>
 					</div>
 				</div>
-				<button className='w-full h-[45px] rounded-md bg-[var(--background-body)] text-[var(--foreground)] mt-[35px] font-semibold text-[13px]'>
+				<button
+					type='button'
+					className='w-full h-[45px] rounded-md bg-[var(--background-body)] text-[var(--foreground)] mt-[35px] font-semibold text-[13px]'
+					onClick={() => {
+						check();
+					}}
+				>
 					Login
 				</button>
 				<div className='flex items-center justify-center mt-[10px] mb-[10px]'>
@@ -91,7 +147,7 @@ export default function SignIn() {
 						Don't have an account?{' '}
 						<Link
 							href='/signUp'
-							className='font-semibold text-[var(--background-body)]'
+							className='font-semibold text-[var(--passive-color)]'
 						>
 							Sign Up
 						</Link>
--- src/app/(pages)/signUp/page.tsx
+++ src/app/(pages)/signUp/page.tsx
@@ -1,9 +1,102 @@
-import { FaFacebook } from 'react-icons/fa6';
+'use client';
+
+import { FaCircleInfo, FaFacebook } from 'react-icons/fa6';
 import google from '@/assets/images/googleLogo.jpg';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 export default function SignUp() {
+	const [password, setPassword] = useState('');
+	const [email, setEmail] = useState('');
+	const [name, setName] = useState('');
+	const easyRegEx = /^[a-zA-Z0-9]{1,5}$/;
+	const mediumRegEx = /^(?=.*[0-9])(?=.*[a-zA-Z])[a-zA-Z0-9]{5,20}$/;
+	const strongRegEx =
+		/^(?=.*[@!#$%^&*()/\\])(?=.*[0-9])(?=.*[a-zA-Z])[@!#$%^&*()/\\a-zA-Z0-9]{8,20}$/;
+	const emailRegEx = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+	let passwordLevel = '';
+
+	function check(): void {
+		if (passwordLevel == 'easy' || passwordLevel == 'none') {
+			let progressPWD = document.querySelector(
+				'.progressPWD'
+			) as HTMLDivElement;
+			let textPWD = document.querySelector('.textPWD') as HTMLParagraphElement;
+			progressPWD.style.width = '0%';
+			textPWD.style.color = '#ff2626';
+			textPWD.innerText = 'Password must contain letters and numbers';
+		}
+
+		let inputControls = document.querySelectorAll(
+			'.inputControl'
+		) as NodeListOf<HTMLDivElement>;
+		inputControls?.forEach((inputControl) => {
+			let input = inputControl.querySelector('.input') as HTMLInputElement;
+			if (input?.value === '') {
+				input.style.borderColor = '#ff0000';
+				let warningIcon = inputControl.querySelector('.warning') as HTMLElement;
+				warningIcon?.classList.remove('opacity-0');
+			}
+		});
+
+		if (!emailRegEx.test(email)) {
+			let warningIconEmail = document.querySelector(
+				'.warning.email'
+			) as HTMLElement;
+			warningIconEmail?.classList.remove('opacity-0');
+		} else {
+			let warningIconEmail = document.querySelector(
+				'.warning.email'
+			) as HTMLElement;
+			warningIconEmail?.classList.add('opacity-0');
+		}
+	}
+
+	function resetInput(event: React.FocusEvent<HTMLInputElement>): void {
+		let input = event.target as HTMLInputElement;
+		let warningIcon = input.parentElement?.querySelector(
+			'.warning'
+		) as HTMLElement;
+		warningIcon?.classList.add('opacity-0');
+		input.style.borderColor = '#aaa';
+	}
+
+	useEffect(() => {
+		let progressPWD = document.querySelector('.progressPWD') as HTMLDivElement;
+		let textPWD = document.querySelector('.textPWD') as HTMLParagraphElement;
+		if (easyRegEx.test(password)) {
+			progressPWD.style.width = '30%';
+			progressPWD.style.backgroundColor = '#ff2626';
+			textPWD.innerText = 'Easy';
+			textPWD.style.color = '#ff2626';
+			passwordLevel = 'easy';
+		}
+		if (mediumRegEx.test(password)) {
+			progressPWD.style.width = '50%';
+			progressPWD.style.backgroundColor = '#1877F2';
+			textPWD.innerText = 'Medium';
+			textPWD.style.color = '#1877F2';
+			passwordLevel = 'medium';
+		}
+		if (strongRegEx.test(password)) {
+			progressPWD.style.width = '70%';
+			progressPWD.style.backgroundColor = '#00bf00';
+			textPWD.innerText = 'Hard';
+			textPWD.style.color = '#00bf00';
+			passwordLevel = 'hard';
+		}
+		if (
+			!easyRegEx.test(password) &&
+			!mediumRegEx.test(password) &&
+			!strongRegEx.test(password)
+		) {
+			progressPWD.style.width = '0%';
+			textPWD.innerText = '';
+			passwordLevel = 'none';
+		}
+	}, [password]);
+
 	return (
 		<main className='bg-[var(--background-body-introduction)] text-[var(--foreground-introduction)] w-full h-full p-[15px]'>
 			<div>
@@ -18,7 +111,7 @@ export default function SignUp() {
 				</p>
 			</div>
 			<form className='mt-[30px] flex flex-col gap-[12px]'>
-				<div className='inputControl flex flex-col gap-[5px]'>
+				<div className='inputControl flex flex-col gap-[5px] relative'>
 					<label htmlFor='name' className='font-semibold text-[14px]'>
 						Name
 					</label>
@@ -28,9 +121,16 @@ export default function SignUp() {
 						type='text'
 						name='name'
 						placeholder='Enter your name'
+						onChange={(event) => {
+							setName(event.target.value);
+						}}
+						onFocus={(event) => {
+							resetInput(event);
+						}}
 					/>
+					<FaCircleInfo className='warning text-red-500 opacity-0 transition-opacity duration-300 w-[20px] h-[20px] absolute right-[18px] top-[42px]' />
 				</div>
-				<div className='inputControl flex flex-col gap-[5px]'>
+				<div className='inputControl flex flex-col gap-[5px] relative'>
 					<label htmlFor='email' className='font-semibold text-[14px]'>
 						Email Address
 					</label>
@@ -40,9 +140,16 @@ export default function SignUp() {
 						type='email'
 						name='email'
 						placeholder='Enter your email'
+						onChange={(event) => {
+							setEmail(event.target.value);
+						}}
+						onFocus={(event) => {
+							resetInput(event);
+						}}
 					/>
+					<FaCircleInfo className='warning email text-red-500 opacity-0 transition-opacity duration-300 w-[20px] h-[20px] absolute right-[18px] top-[42px]' />
 				</div>
-				<div className='inputControl flex flex-col gap-[5px]'>
+				<div className='inputControl flex flex-col gap-[5px] relative'>
 					<label htmlFor='password' className='font-semibold text-[14px]'>
 						Password
 					</label>
@@ -52,19 +159,43 @@ export default function SignUp() {
 						type='password'
 						name='password'
 						placeholder='Enter your password'
+						onChange={(event) => {
+							setPassword(event.target.value);
+						}}
+						onFocus={(event) => {
+							resetInput(event);
+						}}
 					/>
+					<div className='flex w-full items-center justify-between'>
+						<div
+							className='progressPWD w-[0%] h-[6px] rounded transition-all duration-300 bg-red-500'
+							style={{ border: '0px solid #aaa' }}
+						></div>
+						<p className='textPWD font-semibold transition-colors duration-300 text-[13px] text-red-500'></p>
+					</div>
+					<FaCircleInfo className='warning text-red-500 opacity-0 transition-opacity duration-300 w-[20px] h-[20px] absolute right-[18px] top-[42px]' />
 				</div>
 				<div className='inputControl flex gap-[10px] items-center justify-start'>
 					<input
 						type='checkbox'
 						name='conditions'
 						className='w-[20px] h-[20px] rounded-md'
-					/>{' '}
-					<label htmlFor='password' className='font-semibold text-[14px]'>
+						required={true}
+					/>
+					<label
+						htmlFor='password'
+						className='checkBox font-semibold text-[14px]'
+					>
 						I agree to the terms and conditions
 					</label>
 				</div>
-				<button className='w-full h-[45px] rounded-md bg-[var(--background-body)] text-[var(--foreground)] mt-[35px] font-semibold text-[13px]'>
+				<button
+					type='button'
+					className='signButton w-full h-[45px] rounded-md bg-[var(--background-body)] text-[var(--foreground)] transition-colors duration-300 mt-[35px] font-semibold text-[13px]'
+					onClick={() => {
+						check();
+					}}
+				>
 					Sign Up
 				</button>
 				<div className='flex items-center justify-center mt-[10px] mb-[10px]'>
@@ -95,11 +226,11 @@ export default function SignUp() {
 					</button>
 				</div>
 				<div className='w-full flex items-center justify-center'>
-					<p className='w-full text-center mt-[20px] text-[13px]  opacity-60'>
+					<p className='w-full text-center mt-[20px] text-[13px] mb-[20px]  opacity-60'>
 						Already have an account?{' '}
 						<Link
 							href='/signIn'
-							className='font-semibold text-[var(--background-body)]'
+							className='font-semibold text-[var(--passive-color)]'
 						>
 							Login
 						</Link>
