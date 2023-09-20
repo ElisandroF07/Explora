9b5dca9a654eb5311914a6423ed8fcf1a9bd3ed8
ElisandroF07 <f07nobru@gmail.com>
2023-09-20 16:06:42 +0100

    Fix: Customizing dashboard page

--- src/components/dashboard/header.tsx
+++ src/components/dashboard/header.tsx
@@ -1,11 +1,10 @@
-"use client"
+'use client';
 
 import React from 'react';
 import Image from 'next/image';
 import useUserStore from '@/contexts/stores/userStore';
 
 export default function Header() {
-
 	const firstName = useUserStore((state) => state.firstName);
 	const lastName = useUserStore((state) => state.lastName);
 	const pictureUrl = useUserStore((state) => state.pictureUrl);
@@ -16,19 +15,21 @@ export default function Header() {
 				<h1
 					className='font-bold text-[24px] mt-[20px]'
 					style={{ lineHeight: '0px' }}>
-					Wellcome! 👋
+					Dashboard
 				</h1>
 				<p className='font-light text-[14px] mt-[20px] text-[#767676]'>
 					Wellcome {firstName || 'Unknow'} {lastName || 'User'}
 				</p>
 			</div>
-			<Image
-				src={pictureUrl}
-				alt='picture'
-				width={40}
-				height={40}
-				className='rounded-full'
-			/>
+			{pictureUrl && (
+				<Image
+					src={pictureUrl}
+					alt='picture'
+					width={40}
+					height={40}
+					className='rounded-full'
+				/>
+			)}
 		</div>
 	);
 }
